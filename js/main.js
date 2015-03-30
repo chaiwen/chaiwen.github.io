180966d8d82bb65e74a7a5e96464d06e44b09aa9
chaiwen <cc3636@columbia.edu>
2015-03-29 23:56:01 -0400

    fixed some formatting

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var moveS = function(e) {
 	fgc += 10000;
 	bgc -= 10000;
 	//reset
-	
+
 	var ss = document.styleSheets[0];
 	if ("insertRule" in ss) {
 		if (first) {
@@ -16,8 +16,8 @@ var moveS = function(e) {
 			ss.deleteRule(0);
 		}
 		ss.insertRule('::selection {color: #'+fgc.toString(16)+
-		'; background: #'+bgc.toString(16)+';}', 0);
-	
+				'; background: #'+bgc.toString(16)+';}', 0);
+
 	}
 
 	console.log(bgc.toString(16));
@@ -27,58 +27,54 @@ var moveS = function(e) {
 	var x = e.pageX + 'px';
 	var y = e.pageY + 'px';
 	var $div = $('<div>').css({
-		"position": "absolute",
-		"left": x,
-		"top": y,
-		"background-color": "#bbbbbb",
-		"width": "100px",
-		"height": "100px",
-		"float": "left",
-		"padding": "5px",
-		"z-index": z
-	});
+			"position": "absolute",
+			"left": x,
+			"top": y,
+			"background-color": "#bbbbbb",
+			"width": "100px",
+			"height": "100px",
+			"float": "left",
+			"padding": "5px",
+			"z-index": z
+			});
 	$(document.body).append($div);
 	console.log(x);
 	console.log(y);
 	z--;
 }
 
-	
-	function chicken(e) {
-		$(document.body).unbind("mousemove", moveS);
-				/*var selection;
-				if (window.getSelection) {
-					selection = window.getSelection();
-				} else if (document.selection) {
-					selection = document.selection.createRange();
-				}
 
-				selection.toString() != '' && alert('"' + selection.toString() +
-				'" was selected at ' + e.pageX + '/' + e.pageY);*/
-			}
+function chicken(e) {
+	$(document.body).unbind("mousemove", moveS);
+	/*var selection;
+	  if (window.getSelection) {
+	  selection = window.getSelection();
+	  } else if (document.selection) {
+	  selection = document.selection.createRange();
+	  }
 
-			function chicken2(e) {
-				$(document.body).bind("mousemove", moveS);
-								//var selection;
-				//if (window.getSelection) {
-				//	selection = window.getSelection();
-				//} else if (document.selection) {
-				//	selection = document.selection.createRange();
-				//}
+	  selection.toString() != '' && alert('"' + selection.toString() +
+	  '" was selected at ' + e.pageX + '/' + e.pageY);*/
+}
 
-				//alert('lkjlkjk');
+function chicken2(e) {
+	$(document.body).bind("mousemove", moveS);
+	//var selection;
+	//if (window.getSelection) {
+	//	selection = window.getSelection();
+	//} else if (document.selection) {
+	//	selection = document.selection.createRange();
+	//}
 
-				
-			}
+	//alert('lkjlkjk');
 
 
-
-	//document.ready
-		$(function(){
-						$(document.body).bind('mousedown', chicken2);
-						$(document.body).bind("mouseup", chicken);
+}
 
 
-		});
-				
 
+//document.ready
+$(function(){
+	$(document.body).bind('mousedown', chicken2);
+	$(document.body).bind("mouseup", chicken);
+});
