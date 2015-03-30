02d1b1a42a19a07e24ba1f46f7c3ebccfd03207c
chaiwen <cc3636@columbia.edu>
2015-03-29 23:54:29 -0400

    some weird text selection js tests

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
-		$(function(){
-			$(document.body).bind('mouseup', function(e) {
-				var selection;
+var fgc = 0x000000;
+var bgc = 0xffffff;
+var first = true;
+var z = -1;
+var moveS = function(e) {
+	//alert("init");
+	fgc += 10000;
+	bgc -= 10000;
+	//reset
+	
+	var ss = document.styleSheets[0];
+	if ("insertRule" in ss) {
+		if (first) {
+			first = false;
+		} else {
+			ss.deleteRule(0);
+		}
+		ss.insertRule('::selection {color: #'+fgc.toString(16)+
+		'; background: #'+bgc.toString(16)+';}', 0);
+	
+	}
+
+	console.log(bgc.toString(16));
+	console.log(bgc);
+
+
+	var x = e.pageX + 'px';
+	var y = e.pageY + 'px';
+	var $div = $('<div>').css({
+		"position": "absolute",
+		"left": x,
+		"top": y,
+		"background-color": "#bbbbbb",
+		"width": "100px",
+		"height": "100px",
+		"float": "left",
+		"padding": "5px",
+		"z-index": z
+	});
+	$(document.body).append($div);
+	console.log(x);
+	console.log(y);
+	z--;
+}
+
+	
+	function chicken(e) {
+		$(document.body).unbind("mousemove", moveS);
+				/*var selection;
 				if (window.getSelection) {
 					selection = window.getSelection();
 				} else if (document.selection) {
@@ -8,8 +54,31 @@
 				}
 
 				selection.toString() != '' && alert('"' + selection.toString() +
-				'" was selected at ' + e.pageX + '/' + e.pageY);
-			});
+				'" was selected at ' + e.pageX + '/' + e.pageY);*/
+			}
+
+			function chicken2(e) {
+				$(document.body).bind("mousemove", moveS);
+								//var selection;
+				//if (window.getSelection) {
+				//	selection = window.getSelection();
+				//} else if (document.selection) {
+				//	selection = document.selection.createRange();
+				//}
+
+				//alert('lkjlkjk');
+
+				
+			}
+
+
+
+	//document.ready
+		$(function(){
+						$(document.body).bind('mousedown', chicken2);
+						$(document.body).bind("mouseup", chicken);
+
+
 		});
 				
 
