2ac6452ad760c7a92a35827330731bd428da91a0
chaiwen <cc3636@columbia.edu>
2015-11-11 12:02:53 -0500

    rainbow grid background

--- js/main.js
+++ js/main.js
@@ -2,7 +2,56 @@ var fgc = 0x000000;
 var bgc = 0xffffff;
 
 var first = true;
-var z = -1;
+var z = 5;
+
+var colorBG = function() {
+
+
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	ctx.canvas.width = w;
+	ctx.canvas.height = h;
+
+	var numW = w / 20;
+	var numH = h / 20;
+	for (i = 0; i < numW; i++) {
+		for (j = 0; j < numH; j++) {
+			var x = i * 20;
+			var y = j * 20;
+
+			var dc = Math.floor(Math.random() * 0xdddddd) + 0x222222;
+			var dc2 = dc + Math.floor(Math.random() * 0x333333);
+			if (dc2 > 0xffffff) dc2 = 0xffffff;
+
+			var dcolor = dc.toString(16);
+			while (dcolor.length < 6) {
+				dcolor = "0" + dcolor;
+			}
+			dcolor = "#" + dcolor;
+
+			var dcolor2 = dc2.toString(16);
+			while (dcolor2.length < 6) {
+				dcolor2 = "0" + dcolor2;
+			}
+			dcolor2 = "#" + dcolor2;
+
+			var grd = ctx.createLinearGradient(x,y,x + 20,y + 20);
+
+			grd.addColorStop(0,dcolor);
+			grd.addColorStop(1,dcolor2);
+			ctx.fillStyle = grd;
+
+			ctx.fillRect(x,y,x + 20, y + 20);
+
+
+		}
+	}
+	
+}
 var moveS = function(e) {
 	//alert("init");
 	fgc += 10000;
@@ -38,11 +87,11 @@ var moveS = function(e) {
 			"top": y,
 			"background-color": dcolor,
 			"background-repeat": "no-repeat",
-			"width": "25px",
-			"height": "25px",
+			"width": "20px",
+			"height": "20px",
 			"float": "left",
 			"padding": "0px",
-			"z-index": z
+			"z-index": 10
 			});
 	$(document.body).append($div);
 	var $cdiv = $('<div>').css({
@@ -50,7 +99,8 @@ var moveS = function(e) {
 			"background-image": "url('http://www.chaiwen.com/adoptchicken.gif')",
 			"background-repeat": "no-repeat",
 			"width": "25px",
-			"height": "25px"
+			"height": "25px",
+			"z-index": 20
 	});
 	$('#mainContainer').prepend($cdiv);
 
@@ -93,4 +143,5 @@ function chicken2(e) {
 $(function(){
 	$(document.body).bind('mousedown', chicken2);
 	$(document.body).bind("mouseup", chicken);
+	colorBG();
 });
